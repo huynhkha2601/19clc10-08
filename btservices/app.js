6d6ea51aa248786e169670652dbb5859e0ebac3c
hungnguyen157 <91813107+hungnguyen157@users.noreply.github.com>
2021-12-22 08:49:19 +0700

    push luồng

--- btservices/app.js
+++ btservices/app.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { engine } from 'express-handlebars';
-
+import morgan from 'morgan';
 
 import {dirname} from 'path';
 import {fileURLToPath} from 'url';
@@ -11,19 +11,23 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 
 const app = express();
+app.engine('hbs', engine({
+    defaultLayout:'home.hbs',
+}));
 
-app.engine('.hbs', engine());
-app.set('view engine', 'handlebars');
+app.set('view engine', 'hbs');
 app.set('views', './views');
 
 app.get('/', (req, res) => {
-    res.render('home');
+    res.render('main');
 });
 
 app.get('/bs4', (req, res) =>{
     res.sendFile(__dirname + "/bs4.html");
 });
 
+app.use(morgan('dev'));
+const port = 5000;
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 });
