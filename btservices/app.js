1e5a0e2a8ed8e9819febe1c21f2ee666935d30ef
huynhkha2601 <63405972+huynhkha2601@users.noreply.github.com>
2022-01-04 16:34:03 +0700

    sign in/ sign up/ change password form

--- btservices/app.js
+++ btservices/app.js
@@ -1,18 +1,43 @@
 import express from 'express';
-import { engine } from 'express-handlebars';
+import {engine} from "express-handlebars";
+import sections from "express-handlebars-sections";
 import morgan from 'morgan';
-
 import {dirname} from 'path';
 import {fileURLToPath} from 'url';
-import mysql from 'mysql';
-
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 
 const app = express();
+app.use('/public', express.static('public'));
+app.use(express.urlencoded({
+    extended:true
+}));
+
 app.engine('hbs', engine({
     defaultLayout:'home.hbs',
+}));
+
+app.engine('hbs', engine({
+    defaultLayout: 'home.hbs',
+    helpers: {
+        section: sections(),
+        formatMoney(val) {
+            return val.toLocaleString('vi', {
+                style: 'currency', currency: 'VND'
+            });
+        },
+        formatDateTime(d) {
+            return d.toLocaleString('vi');
+        },
+        equals(variable, statements, value) {
+            if (statements === '=') {
+                return (variable === value);
+            }
+            if (statements === '!=')
+                return (variable !== value);
+        }
+    }
 }));
 
 app.set('view engine', 'hbs');
@@ -30,6 +55,10 @@ app.get('/register', (req, res) => {
     res.render('./vwAccounts/register',{layout : 'accounts.hbs'}) ;
 });
 
+app.get('/forget', (req, res) => {
+    res.render('./vwAccounts/forgetpw',{layout : 'accounts.hbs'}) ;
+});
+
 app.get('/Product', (req, res) => {
     res.render('./vwProducts/index',{layout : 'home.hbs'}) ;
 });
@@ -38,9 +67,7 @@ app.get('/Product/detail', (req, res) => {
     res.render('./vwProducts/details',{layout : 'home.hbs'}) ;
 });
 
-app.use('/public', express.static('public'));
-app.use(morgan('dev'));
-const port = 5000;
+let port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 });
